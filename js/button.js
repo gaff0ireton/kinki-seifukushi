e2dc6af81e22e08066a1eed235b3af980111aa53
gaff0ireton <149355314+gaff0ireton@users.noreply.github.com>
2024-01-15 14:01:08 +0900

    Delete js directory

--- js/button.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelector('.items');
-const button = document.getElementById('buttonM');
-
-button.addEventListener('click', function () {
-    items.classList.toggle('open');
-});
-
